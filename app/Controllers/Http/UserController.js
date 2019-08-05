9835f1b2acb8f298fb2b66299230d32ef940ec2d
Tpk <admin@tcgforum.pl>
2019-08-05 20:00:38 +0200

    Create index, store methods in UserController

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,9 +1,30 @@
 'use strict'
 
+const User = use('App/Models/User')
+
 class UserController {
 
-    async index({ response }) {
-        response.json({ hello: "World" })
+    async index() {
+        return await User.all()
+    }
+
+    async store({ request, response }) {
+        try {
+            const { username, email, password } = request.post()
+            // TODO: Validate data befere send to database
+
+            const user = new User()
+            user.username = username
+            user.email = email
+            user.password = password
+
+            await user.save()
+
+            return response.status(201).json({ message: 'User created successfully' })
+        } catch (e) {
+            // TODO: Throw exception
+            return response.status(400).json({ message: 'Something went wrong' })
+        }
     }
 
 }
